56af4feb4b3df4560d4b615fb4fb82a9ab76097f
mustafizarif <mustafizarif17@gmail.com>
2026-07-18 17:33:20 +0600

    Refactor Fibonacci function and update main logic
    Refactored Fibonacci function to return nth number and updated main to print series.

--- fibonacci_Recursion.c
+++ fibonacci_Recursion.c
@@ -1,15 +1,25 @@
-#include<stdio.h>
-int fibonacci(int n,int x,int y){
-    if(n==0)
-    return 0;
-    printf("%d ",x);
-    return fibonacci(n-1,y,x+y);
+#include <stdio.h>
+
+// Returns the nth Fibonacci number
+int fibonacci(int n) {
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
-int main(){
+
+int main() {
     int n;
-    printf("Enter the number:");
-    scanf("%d",&n);
-    printf("The fibonacci seris is:");
-    fibonacci(n,0,1);
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    printf("The Fibonacci series is:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", fibonacci(i));
+    }
+    printf("\n");
+
     return 0;
 }
